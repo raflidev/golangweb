46ca57385593c911258e7ee2d4d582d59d4b227a
Rafli Ramadhan <rafliramdhn@gmail.com>
2022-01-17 15:44:45 +0700

    finish the course

--- handler/handler.go
+++ handler/handler.go
@@ -70,3 +70,75 @@ func ProductHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+
+func PostGet(w http.ResponseWriter, r *http.Request) {
+	method := r.Method
+
+	switch method {
+	case "GET":
+		w.Write([]byte("ini adalah get"))
+	case "POST":
+		w.Write([]byte("ini adalah post"))
+	default:
+		http.Error(w, "This is just Bug, just calm :)", http.StatusBadRequest)
+	}
+}
+
+func Form(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		tmpl, err := template.ParseFiles(path.Join("views", "form.html"), path.Join("views", "layout.html"))
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "This is just Bug, just calm :)", http.StatusInternalServerError)
+			return
+		}
+
+		err = tmpl.Execute(w, nil)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "This is just Bug, just calm :)", http.StatusInternalServerError)
+			return
+		}
+
+		return
+	}
+
+	http.Error(w, "This is just Bug, just calm :)", http.StatusBadRequest)
+}
+
+func Process(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "This is just Bug, just calm :)", http.StatusInternalServerError)
+			return
+		}
+
+		name := r.Form.Get("name")
+		message := r.Form.Get("message")
+
+		data := map[string]interface{}{
+			"name":    name,
+			"message": message,
+		}
+
+		tmpl, err := template.ParseFiles(path.Join("views", "result.html"), path.Join("views", "layout.html"))
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "This is just Bug, just calm :)", http.StatusInternalServerError)
+			return
+		}
+
+		tmpl.Execute(w, data)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "This is just Bug, just calm :)", http.StatusInternalServerError)
+			return
+		}
+
+		return
+	}
+
+	http.Error(w, "This is just Bug, just calm :)", http.StatusInternalServerError)
+}
